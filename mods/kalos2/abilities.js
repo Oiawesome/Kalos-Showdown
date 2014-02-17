618dfddba6a97c831242c8a4d4704faeedf7b415
Pikachuun <pikachuun@gmail.com>
2014-02-17 10:03:20 -0500

    Made Description changes.
    Also, I made a change to corrosive poison, but it's incomplete.

--- mods/kalos2/abilities.js
+++ mods/kalos2/abilities.js
@@ -1,7 +1,7 @@
 exports.BattleAbilities = {
         "honeygather": {
-                desc: "If it is not already holding an item, this Pokemon may find and be holding Honey after a battle.",
-                shortDesc: "No competitive use.",
+                desc: "When this Pokemon is holding Honey, its attacks do 1.25x damage.",
+                shortDesc: "When this Pokemon is holding Honey, its attacks do 1.25x damage.",
                 onModifyAtkPriority: 5,
                 onModifyAtk: function(atk, attacker, defender, move) {
                         if (attacker.item.id === 'honey') {
@@ -14,13 +14,13 @@ exports.BattleAbilities = {
                 rating: 0,
                 num: 118
         },
-		"corrosivepoison": {
-		desc: "This Pokemon has the ability to hit Ghost-type Pokemon with Normal-type and Fighting-type moves. Effectiveness of these moves takes into account the Ghost-type Pokemon's other weaknesses and resistances.",
-		shortDesc: "This Pokemon can hit Ghost-types with Normal- and Fighting-type moves.",		
-		onFoeModifyPokemon: function(pokemon) {
-			if (pokemon.hasType('steel')) {
-			return effectiveness + 2;
-		    }
+	"corrosivepoison": {
+		desc: "This Pokemon has the ability to hit Steel-type Pokemon super effectively with Poison-type moves. Effectiveness of these moves takes into account the Steel-type Pokemon's other weaknesses and resistances.",
+		shortDesc: "This Pokemon can hit Steel-types super effectively with Poison-type moves.",		
+		onModifyMove: function(move) {
+			if (move.type in {'Poison':1}) {
+				move.affectedByImmunities = false;
+			}
 		},
 		id: "corrosivepoison",
 		name: "Corrosive Poison",
@@ -28,8 +28,8 @@ exports.BattleAbilities = {
 		num: -113
 	},
 	"dimensionwarp": {
-		desc: "When this Pokemon enters the battlefield, the weather becomes Sunny Day (for 5 turns normally, or 8 turns while holding Heat Rock).",
-		shortDesc: "On switch-in, the weather becomes Sunny Day.",
+		desc: "When this Pokemon enters the battlefield, Trick Room is set up (for 7 turns).",
+		shortDesc: "On switch-in, Trick Room is set up.",
 		onStart: function(source) {
 				this.addPseudoWeather('trickroom');
 		},
@@ -39,8 +39,8 @@ exports.BattleAbilities = {
 		num: -70
 	},
 	"snowslide": {
-		desc: "This Pokemon's Speed is doubled if the weather is Sandstorm. This Pokemon is also immune to residual Sandstorm damage.",
-		shortDesc: "If Sandstorm is active, this Pokemon's Speed is doubled; immunity to Sandstorm.",
+		desc: "This Pokemon's Speed is doubled if the weather is Hail. This Pokemon is also immune to residual Hail damage.",
+		shortDesc: "If Hail is active, this Pokemon's Speed is doubled; immunity to Hail.",
 		onModifySpe: function(speMod, pokemon) {
 			if (this.isWeather('hail')) {
 				return this.chain(speMod, 2);
@@ -55,8 +55,8 @@ exports.BattleAbilities = {
 		num: -146
 	},	
         "ironfist": {
-                desc: "This Pokemon receives a 20% power boost for the following attacks: Bullet Punch, Comet Punch, Dizzy Punch, Drain Punch, Dynamicpunch, Fire Punch, Focus Punch, Hammer Arm, Ice Punch, Mach Punch, Mega Punch, Meteor Mash, Shadow Punch, Sky Uppercut, and Thunderpunch. Sucker Punch, which is known Ambush in Japan, is not boosted.",
-                shortDesc: "This Pokemon's punch-based attacks do 1.2x damage. Sucker Punch is not boosted.",
+                desc: "This Pokemon receives a 30% power boost for the following attacks: Bullet Punch, Comet Punch, Dizzy Punch, Drain Punch, Dynamicpunch, Fire Punch, Focus Punch, Hammer Arm, Ice Punch, Mach Punch, Mega Punch, Meteor Mash, Shadow Punch, Sky Uppercut, and Thunderpunch. Sucker Punch, which is known Ambush in Japan, is not boosted.",
+                shortDesc: "This Pokemon's punch-based attacks do 1.3x damage. Sucker Punch is not boosted.",
                 onBasePowerPriority: 8,
                 onBasePower: function(basePower, attacker, defender, move) {
                         if (move.isPunchAttack) {
@@ -81,8 +81,8 @@ exports.BattleAbilities = {
 		num: -222
 	},		
         "burstingjets": {
-                desc: "Raises the user's Attack stat by two stages when a stat is lowered, including the Attack stat. This does not include self-induced stat drops like those from Close Combat.",
-                shortDesc: "This Pokemon's Attack is boosted by 2 for each of its stats that is lowered by a foe.",
+                desc: "Raises the user's Speed stat by two stages when a stat is lowered, including the Speed stat. This does not include self-induced stat drops like those from Close Combat. This Pokemon also cannot become paralyzed.",
+                shortDesc: "This Pokemon's Speed is boosted by 2 for each of its stats that is lowered by a foe. This Pokemon cannot be paralyzed. Gaining this Ability while paralyzed cures it.",
                 onAfterEachBoost: function(boost, target, source) {
                         if (!source || target.side === source.side) {
                                 return;
@@ -111,8 +111,8 @@ exports.BattleAbilities = {
                 num: -5
         },
         "defiant": {
-                desc: "Raises the user's Attack stat by two stages when a stat is lowered, including the Attack stat. This does not include self-induced stat drops like those from Close Combat.",
-                shortDesc: "This Pokemon's Attack is boosted by 2 for each of its stats that is lowered by a foe.",
+                desc: "Raises the user's Attack stat by two stages when a stat is lowered, including the Attack stat. This does not include self-induced stat drops like those from Close Combat. This Pokemon also cannot become burned.",
+                shortDesc: "This Pokemon's Attack is boosted by 2 for each of its stats that is lowered by a foe. This Pokemon cannot be burned. Gaining this Ability while paralyzed cures it.",
                 onAfterEachBoost: function(boost, target, source) {
                         if (!source || target.side === source.side) {
                                 return;
